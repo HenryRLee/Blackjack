674306aa50aec07c37f74c334a4c5702ac6c0932
HenryLee <lee@Lee-HP-431>
2013-10-23 16:20:24 +0800

    Tune EnhancedStrategy

--- Blackjack/EnhancedStrategy.cpp
+++ Blackjack/EnhancedStrategy.cpp
@@ -46,7 +46,8 @@ int EnhancedStrategy::MakeDecision(Hand hand, bitset <5> allowSet, Table table)
 		if (allowSet[SURRENDER] == 1)
 			return SURRENDER;
 
-		for (int i=0; i<vCard.size(); i++)
+		/* Skip Dealer's card */
+		for (int i=1; i<vCard.size(); i++)
 		{
 			int value = vCard[i].GetValue();
 
@@ -58,6 +59,48 @@ int EnhancedStrategy::MakeDecision(Hand hand, bitset <5> allowSet, Table table)
 			return HIT;
 		else
 			return STAND;
+	}
+	/* 14 vs 10 */
+	else if ((value == 14) && !soft && (iDealerFaceup == 10))
+	{
+		/* HIT vs SURRENDER */
+		const double original = -0.005379;
+		const double delta[12] = 
+		{
+			0,			/* Not used */
+			0,			/* Not used */
+			-0.000442,	/* 2 */
+			-0.000856,	/* 3 */
+			-0.001549,	/* 4 */
+			-0.002249,	/* 5 */
+			-0.003667,	/* 6 */
+			-0.004789,	/* 7 */
+			0.001482,	/* 8 */
+			0.001890,	/* 9 */
+			0.002269,	/* 10 */
+			0.001648,	/* A */
+		};
+		UsedCard vUsedCard = table.ShowUsedCards();
+		vector <Card> vCard = vUsedCard.ShowCards();
+		double ev = original;
+
+		if (allowSet[SURRENDER] == 0)
+			return HIT;
+
+		/* Skip Dealer's card */
+		for (int i=1; i<vCard.size(); i++)
+		{
+			int value = vCard[i].GetValue();
+
+			if ((value >= 2) && (value <= 11))
+				ev += delta[value];
+		}
+
+		if (ev >= 0)
+			return HIT;
+		else
+			return SURRENDER;
+
 	}
 	else
 	{
