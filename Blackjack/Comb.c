47c6e20fd40b0c68f9759af3b039925dad8c52cf
HenryLee <lee@Lee-HP-431>
2014-02-10 15:35:00 +0800

    Modify comments in Comb

--- Blackjack/Comb.c
+++ Blackjack/Comb.c
@@ -1,9 +1,9 @@
 /*
- * This program calculates the number of combinations of making a number num
- * from {1 2 .. choice} by addition.
+ * This program calculates the number of combinations of reaching an integer num
+ * by adding integers from the set {1 2 .. choices}.
  * For example, to count the possible combinations of cards used from 16 to
- * 21, we can let num be 5 and choice be 10. Thus the result is 7.
- * All combinations in this example is listed below.
+ * 21, we can let num be 5 and choices be 10. The result is 7.
+ * All combinations in this example are listed below.
  *    A 2 3 4 5 6 7 8 9 T
  *    5 0 0 0 0 0 0 0 0 0
  *    3 1 0 0 0 0 0 0 0 0
@@ -16,11 +16,11 @@
 
 #include <stdio.h>
 
-int comb(int num, int choice)
+int comb(int num, int choices)
 {
 	int sum = 0;
 
-	if ((num == 1) || (num == 0) || (choice == 1))
+	if ((num == 1) || (num == 0) || (choices == 1))
 	{
 		return 1;
 	}
@@ -28,7 +28,7 @@ int comb(int num, int choice)
 	{
 		for (int i=0; i<num; i++)
 		{
-			if ((num-i) > choice)
+			if ((num-i) > choices)
 				continue;
 
 			sum += comb(i, num-i);
@@ -45,4 +45,6 @@ int main(int argc, char *argv[])
 		sum = comb(atoi(argv[1]), atoi(argv[2]));
 		printf("%d\n", sum);
 	}
+
+	return 0;
 }
